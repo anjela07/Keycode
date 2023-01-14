52c9f38ba0107a0937092b0b4529ba5944b28ff9
Angela <116427072+anjela07@users.noreply.github.com>
2023-01-14 19:51:00 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let insert = document.getElementById("insert");
+
+document.addEventListener("keydown", (event) => {
+  insert.innerHTML = `
+  <div class="key">
+   ${event.key}
+   <small> Key </small></div>
+  <div class="keyC"> 
+  ${event.keyCode}
+   <small> KeyCode </small></div>
+  <div class="code">
+   ${event.code}
+    <small> Code </small></div>`;
+});
